9e91a5b64edf0f9ad5f255034e073c16df1db90e
Nickollas <nickollassilva90@gmail.com>
2024-08-04 14:35:28 -0300

    atribuindo a função de pesquisar filmes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-import { Key } from "./key.js";
-
 const filmes = document.querySelector("#filmes");
-const key = new Key();
+const buscarBtn = document.querySelector(".lupa");
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + key.token,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNWEyYTQzMjM5OTIxZmJlZWQ3ZTY0M2FhODk0ZWU4NiIsIm5iZiI6MTcyMjcwOTI4MC4xMzE2MzEsInN1YiI6IjY2YWU3MzcwOTlkOGU2MjA5MWQ4ZWIyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8ievS79oI7PKX0RtMujZYCijVCY6QvH5qmC3I-Yx9CA",
   },
 };
 
@@ -16,33 +15,57 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
-    response.results.forEach((movie) => {
-      filmes.innerHTML += `
-      
-    <div class="filme">
-        <img
-          src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path}"
-          alt="imagem do filme ${movie.original_title}"
-          class="imagemDoFilme"
-        />
-        <div class="informacoesDoFilme">
-          <p>${movie.original_title}</p>
-          <div class="feedback">
-            <div class="avaliacao">
-              <i class="fa-solid fa-star"></i>
-              <p>${movie.vote_average.toFixed(2)}</p>
-            </div>
-            <div class="favoritar">
-              <i class="fa-solid fa-heart"></i>
-              <p>Favoritar</p>
-            </div>
-          </div>
-        </div>
-        <p class="descricaoDoFilme">
-          ${movie.overview}
-        </p>
-      </div>
-`;
-    });
+    renderizaFilmes(response);
   })
   .catch((err) => console.error(err));
+
+buscarBtn.addEventListener("click", (e) => {
+  console.log("Clicou");
+  e.preventDefault();
+  const inputValue = buscarBtn.previousElementSibling.value;
+  pesquisarFilmes(inputValue);
+});
+
+function pesquisarFilmes(nomeDoFilme) {
+  filmes.innerHTML = "";
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${nomeDoFilme}&include_adult=false&language=en-US&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      renderizaFilmes(response);
+    })
+    .catch((err) => console.error(err));
+}
+function renderizaFilmes(response) {
+  response.results.forEach((movie) => {
+    filmes.innerHTML += `
+    
+  <div class="filme">
+      <img
+        src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path}"
+        alt="imagem do filme ${movie.original_title}"
+        class="imagemDoFilme"
+      />
+      <div class="informacoesDoFilme">
+        <p>${movie.original_title}</p>
+        <div class="feedback">
+          <div class="avaliacao">
+            <i class="fa-solid fa-star"></i>
+            <p>${movie.vote_average.toFixed(2)}</p>
+          </div>
+          <div class="favoritar">
+            <i class="fa-solid fa-heart"></i>
+            <p>Favoritar</p>
+          </div>
+        </div>
+      </div>
+      <p class="descricaoDoFilme">
+        ${movie.overview}
+      </p>
+    </div>
+`;
+  });
+}
